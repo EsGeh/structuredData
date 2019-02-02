d40e710b088a4ff6af663c5ee10628c67bd447f8
EsGeh <SamuelGfroerer@googlemail.com>
2019-02-02 12:24:21 +0100

    depend on specific commit of fishshell-cmd-opts

--- scripts/install_deps.fish
+++ scripts/install_deps.fish
@@ -9,3 +9,5 @@ rm -rf -v $DEP_DIR
 mkdir -p -v $DEP_DIR
 cd $DEP_DIR
 git clone git@github.com:EsGeh/fishshell-cmd-opts.git
+cd fishshell-cmd-opts/
+git checkout e446daf1741b416ecb83e4741b4cb7f99645bc11
