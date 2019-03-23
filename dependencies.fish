7230f11c24f4c509eab8a4d5772fd107fa58f116
EsGeh <SamuelGfroerer@googlemail.com>
2019-03-23 12:12:08 +0100

    standardized dependency management using https://github.com/EsGeh/git_deps

---
+++ dependencies.fish
@@ -0,0 +1,15 @@
+#!/bin/fish
+
+########################################
+# dependencies of this repository
+########################################
+
+set dependencies \
+	fishshell_cmd_opts
+
+# for every entry in $dependencies:
+# 	name uri version [init-command]
+set fishshell_cmd_opts \
+	fishshell-cmd-opts  \
+	git@github.com:EsGeh/fishshell-cmd-opts.git	\
+	e446daf1741b416ecb83e4741b4cb7f99645bc11
--- scripts/init.fish
+++ scripts/init.fish
@@ -4,91 +4,25 @@ set BASE_DIR (dirname (readlink -m (status filename)))/..
 set SCRIPTS_DIR (dirname (readlink -m (status filename)))
 set DEP_DIR_DEFAULT "$BASE_DIR/dependencies"
 
-set fishshell_cmd_opts_version e446daf1741b416ecb83e4741b4cb7f99645bc11
+set get_deps_version "6b5b4e36940e67c9ff05e75e027000fbc1462e90"
 
+mkdir -p "$DEP_DIR_DEFAULT"
 
-# (syntax: short/long/description)
-set options \
-	'h/help' \
-	"d/deps-dir="
-
-#################################################
-# functions
-#################################################
-
-function print_help
-	echo "usage: "(status -f)" [OPTIONS...]"
-	echo
-	echo "OPTIONS:"
-	echo "  -h | --help"
-	echo "  -d | --deps-dir"
+set get_deps_uri "https://github.com/EsGeh/git_deps/raw/$get_deps_version/get_deps.fish"
+begin
+	wget \
+		--output-document="$DEP_DIR_DEFAULT/get_deps.fish" \
+			"$get_deps_uri" 2>/dev/null
+	and chmod u+x "$DEP_DIR_DEFAULT/get_deps.fish"
 end
-
-#################################################
-# cmd line interface
-#################################################
-
-# parse command line arguments:
-argparse $options -- $argv 2>/dev/null
 or begin
-	print_help
+	echo "ERROR while downloading '$repo_uri'"
 	exit 1
 end
-if set -q _flag_h
-	print_help
-	exit 0
-else
-	if set -q _flag_deps_dir
-		set deps_dir $_flag_deps_dir
-	end
+
+
+and begin
+	eval "$DEP_DIR_DEFAULT/get_deps.fish" $argv
 end
 
-#################################################
-# actual script
-#################################################
-
-if set -q deps_dir
-	set DEP_DIR $deps_dir
-else
-	set DEP_DIR $DEP_DIR_DEFAULT
-end
-
-
-mkdir -p -v $DEP_DIR
-
-function is_up_to_date
-	if [ -d $DEP_DIR/fishshell-cmd-opts ]
-		cd "$DEP_DIR"/fishshell-cmd-opts
-		if [ (git rev-parse HEAD) = $fishshell_cmd_opts_version ]
-			cd -
-			return 0
-		end
-		cd -
-	end
-	return 1
-end
-
-if is_up_to_date
-	echo "fishshell-cmd-opts version $fishshell_cmd_opts_version found"
-else
-	if [ "$DEP_DIR" = "$DEP_DIR_DEFAULT" ]
-		rm -rf -v $DEP_DIR
-		mkdir -p -v $DEP_DIR
-
-		cd "$DEP_DIR"
-		# cmd line utils:
-		git clone git@github.com:EsGeh/fishshell-cmd-opts.git
-		cd fishshell-cmd-opts/
-		git checkout $fishshell_cmd_opts_version
-	else
-		echo "WARNING: incompatible versions!"
-	end
-end
-
-if [ $DEP_DIR != $DEP_DIR_DEFAULT ]
-	rm $SCRIPTS_DIR/utils/cmd_args.fish
-	ln -s (readlink -m $DEP_DIR)/fishshell-cmd-opts/cmd_args.fish $SCRIPTS_DIR/utils/cmd_args.fish
-end
-
-cd "$BASE_DIR"
 eval "$SCRIPTS_DIR/utils/run_autotools.fish"
