ad27977ea4345defabc13358ec0cffd5094721b6
EsGeh <SamuelGfroerer@googlemail.com>
2019-03-24 14:16:33 +0100

    refer to dependencies via "http" instead of ssh

--- dependencies.fish
+++ dependencies.fish
@@ -12,7 +12,7 @@ set dependencies \
 # 	name uri version [init-command]
 set fishshell_cmd_opts \
 	fishshell-cmd-opts  \
-	git@github.com:EsGeh/fishshell-cmd-opts.git	\
+	https://github.com/EsGeh/fishshell-cmd-opts.git \
 	e446daf1741b416ecb83e4741b4cb7f99645bc11
 
 set c_container_types \
