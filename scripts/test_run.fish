0e26a99e2f1969a3ecfeedfe407d856e13af3998
EsGeh <SamuelGfroerer@googlemail.com>
2019-04-27 11:13:07 +0200

    added test to reveal performance issues in "sdPackMatch"

--- scripts/test_run.fish
+++ scripts/test_run.fish
@@ -6,9 +6,11 @@ set SCRIPTS_DIR (dirname (readlink -m (status filename)))
 set doc_dir $BASE_DIR/doc
 
 
-pd -noaudio \
+# (enabled audio, to test performance issues)
+pd \
 	-noprefs \
 	-stderr \
+	-jack \
 	-lib zexy \
 	-path "$doc_dir" \
 	-lib "structuredDataC" \
