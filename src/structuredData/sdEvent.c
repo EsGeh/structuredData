b110fba57e6abec892c1a7b9861dc0fbce0499e8
EsGeh <SamuelGfroerer@googlemail.com>
2019-08-15 10:11:47 +0200

    sdUnEvent dumps "unknown" parameters at second rightmost outlet.

--- src/structuredData/sdEvent.c
+++ src/structuredData/sdEvent.c
@@ -2,6 +2,7 @@
 
 #include "m_pd.h"
 
+#include "DynArray.h"
 
 static t_class* event_class;
 static t_class* unevent_class;
@@ -175,11 +176,15 @@ void event_input(
 // unevent
 //----------------------------------
 
+DECL_DYN_ARRAY(IntArray,t_int, getbytes, freebytes)
+DEF_DYN_ARRAY(IntArray,t_int, getbytes, freebytes)
+
 typedef struct s_unevent {
   t_object x_obj;
 	unsigned int paramsCount;
 	t_symbol** params;
 	t_outlet** outlets;
+	t_outlet* otherParamsOutlet;
 	t_outlet* typeOutlet;
 } t_unevent;
 
@@ -267,6 +272,9 @@ void* unevent_init(
 		}
 	}
 
+	x->otherParamsOutlet =
+		outlet_new( & x->x_obj, &s_symbol);
+
 	x->typeOutlet =
 		outlet_new( & x->x_obj, &s_symbol);
 
@@ -308,6 +316,11 @@ void unevent_input(
 	// a mapping from    index(x->params) to   index_of_pack_in_incoming_msg (-1, if not found)
 	t_int* param_indices;
 	param_indices = getbytes( sizeof( t_int ) * x->paramsCount );
+	// array of "other" packs:
+	IntArray otherPacks;
+	IntArray_init( &otherPacks );
+
+	// init indices:
 	for( int i=0; i< x->paramsCount; i++)
 		param_indices[i] = -1;
 
@@ -327,15 +340,48 @@ void unevent_input(
 			}
 			t_symbol* type = atom_getsymbol( &argv[pos+0] );
 			unsigned int count = atom_getint( &argv[pos+1] );
+			BOOL found = FALSE;
 			for( int i = 0; i < x->paramsCount; i++ )
 			{
 				if( x->params[i] == type )
+				{
 					param_indices[i] = pos;
+					found = TRUE;
+				}
+			}
+			if( ! found )
+			{
+				IntArray_append( &otherPacks, pos );
 			}
 			pos += (count + 2) ;
 		}
 	}
 
+	// output "other" parameters:
+	{
+		for(int i = 0; i<IntArray_get_size( &otherPacks ); i++)
+		{
+			t_int pos = IntArray_get_array( &otherPacks )[i];
+			if(
+				argv[pos+0].a_type != A_SYMBOL
+				|| argv[pos+1].a_type != A_FLOAT
+				|| pos + 1 + atom_getint( &argv[pos+1] ) >= argc
+			)
+			{
+				pd_error(x, "at pos %i: invalid sdPack", (int )pos);
+				IntArray_exit( & otherPacks );
+				freebytes( param_indices, sizeof( t_int ) * x->paramsCount );
+				return;
+			}
+			unsigned int count = atom_getint( &argv[pos+1] );
+			outlet_list(
+				x->otherParamsOutlet,
+				& s_list,
+				count+2,
+				& argv[ pos+0 ]
+			);
+		}
+	}
 	{
 		// output parameters:
 		for(int i = x->paramsCount-1; i>= 0; i--)
@@ -343,6 +389,7 @@ void unevent_input(
 			t_symbol* currentSym = x->params[i];
 			t_outlet* currentOutlet = x->outlets[i];
 
+			// unconditionally dump at an "all" outlet
 			if( currentSym == gensym("all") )
 			{
 				unsigned int pos = 2;
@@ -355,6 +402,8 @@ void unevent_input(
 					)
 					{
 						pd_error(x, "at pos %i: invalid sdPack", pos);
+						IntArray_exit( & otherPacks );
+						freebytes( param_indices, sizeof( t_int ) * x->paramsCount );
 						return;
 					}
 					unsigned int count = atom_getint( &argv[pos+1] );
@@ -369,9 +418,17 @@ void unevent_input(
 					pos += (count + 2) ;
 				}
 			}
-			else if( param_indices[i] == -1 )
+			if( param_indices[i] == -1 )
 			{
-				// if there is no such value, just output nothing (no error)
+				// if there is no such value, just output at the otherParamsOutlet:
+				/*
+				outlet_list(
+					x->otherParamsOutlet,
+					& s_list,
+					min(atom_getint( &argv[pos+1] ), argc-pos ),
+					& argv[pos]
+				);
+				*/
 				/*
 				char buf[256];
 				t_atom current_sym_atom;
@@ -391,6 +448,7 @@ void unevent_input(
 			}
 		}
 	}
+	IntArray_exit( & otherPacks );
 	freebytes( param_indices, sizeof( t_int ) * x->paramsCount );
 }
 
